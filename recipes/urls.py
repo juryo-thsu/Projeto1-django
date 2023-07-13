bcd5adcf45f038d9b9f43eb7a3d100e982c3d663
Julio Cesar <cesarjulio646@gmail.com>
2023-07-13 18:38:42 -0300

    url att

---
+++ recipes/urls.py
@@ -0,0 +1,9 @@
+from recipes.views import home, contato, sobre
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
+]
